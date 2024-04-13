85fabe1de805c5706a6f2454f23e44e5a32ce4a8
kelly6bf <kelly6bf@gmail.com>
2024-04-14 01:55:54 +0900

    Init Project

---
+++ src/main/java/toy/kellymarket/KellyMarketApplication.java
@@ -0,0 +1,13 @@
+package toy.kellymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KellyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KellyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/kellymarket/KellyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.kellymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KellyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
